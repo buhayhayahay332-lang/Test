369d98649eb546d884bad1c238c3a9d30614de45
buhayhayahay332-lang <buhayhayahay332@gmail.com>
2026-02-20 07:07:30 -0800

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int number;
+
+    cout << "Number: 67 ";
+    cin >> number;
+
+    cout << "SIX SEVEN!!:" << number << endl;
+
+    return 0;
+}
